2efb4f9f2278895a9c53775a2352c50a7f13ba15
Teddy Archibald <107772813+teddyvere@users.noreply.github.com>
2024-09-17 18:19:21 -0400

    webpage created

--- musicapp/api/views.py
+++ musicapp/api/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def main(request):
+    return HttpResponse("<h1>Hello, World! This is the main page of the Music App.</h1>")
